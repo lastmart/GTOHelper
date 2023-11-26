ea32bf4bc990e3bc3e1010a092007fa3ff1deb05
Ivan Fedorov <fedivmi@gmail.com>
2023-11-26 13:11:28 +0500

    initial commit

---
+++ app/src/main/java/com/gtohelper/presentation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.gtohelper.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gtohelper.R
+import com.gtohelper.presentation.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
