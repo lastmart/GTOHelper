032e29e0aec0a23c3ccc6d023acb843015bee813
Ivan Fedorov <fedivmi@gmail.com>
2023-12-19 21:03:07 +0500

    Fixed dark mode

--- app/src/main/java/com/gtohelper/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/gtohelper/presentation/ui/main/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gtohelper.presentation.ui.main
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var database: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
