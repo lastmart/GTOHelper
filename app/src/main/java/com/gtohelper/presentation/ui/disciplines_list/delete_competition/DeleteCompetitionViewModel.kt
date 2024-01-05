fa1b5418b70ed7bf4da5fb4268702299e1cc3229
Xoka74 <ya.vladimir565@mail.ru>
2024-01-05 05:56:21 +0500

    Deleted unnecessary fragments

--- app/src/main/java/com/gtohelper/presentation/ui/disciplines_list/delete_competition/DeleteCompetitionViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.gtohelper.presentation.ui.disciplines_list.delete_competition
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class DeleteCompetitionViewModel @Inject constructor() : ViewModel() {
-}
--- app/src/main/java/com/gtohelper/presentation/ui/util/FragmentExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.gtohelper.presentation.ui.util
-
-import androidx.fragment.app.Fragment
-
-fun Fragment?.runOnUiThread(action: () -> Unit) {
-    this ?: return
-    if (!isAdded) return
-    activity?.runOnUiThread(action)
-}
