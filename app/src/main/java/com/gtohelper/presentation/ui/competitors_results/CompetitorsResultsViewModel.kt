d284cc7d800d0c96e2c00e64eb0913b9f3d82a9b
Ivan Fedorov <fedivmi@gmail.com>
2024-01-09 19:06:56 +0500

    Added progress bar when loading competitors results

--- app/src/main/java/com/gtohelper/presentation/ui/competitors_results/CompetitorsResultsViewModel.kt
+++ app/src/main/java/com/gtohelper/presentation/ui/competitors_results/CompetitorsResultsViewModel.kt
@@ -6,13 +6,17 @@ import androidx.lifecycle.viewModelScope
 import com.gtohelper.domain.models.Competitor
 import com.gtohelper.domain.repository.CompetitorResultsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,32 +29,35 @@ class CompetitorsResultsViewModel @Inject constructor(
     private val _searchQuery = MutableStateFlow("")
     val searchQuery = _searchQuery.asStateFlow()
 
-    val uiState: StateFlow<CompetitorsResultsUIState> =
-        competitorResultsRepository.getCompetitorsWithSportResults(competitionId)
-            .combine(_searchQuery) { list, query ->
+    @OptIn(FlowPreview::class)
+    val uiState: StateFlow<CompetitorsResultsUIState> = _searchQuery
+        .debounce(500L)
+        .onEach { _isLoading.update { true } }
+        .combine(competitorResultsRepository.getCompetitorsWithSportResults(competitionId)) { query, list ->
 
-                if (query.isBlank())
-                    return@combine list
+            if (query.isBlank())
+                return@combine list
 
-                val newList = list.filter { (competitor, result) ->
-                    doesCompetitorMatchQuery(competitor, query)
-                }
-
-                return@combine newList
+            val newList = list.filter { (competitor, result) ->
+                doesCompetitorMatchQuery(competitor, query)
             }
-            .map { competitorsResults ->
-                CompetitorsResultsUIState(
-                    competitorsResults = competitorsResults.sortedByDescending { it.second }
-                )
-            }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(5_000),
-                initialValue = CompetitorsResultsUIState(listOf())
+
+            return@combine newList
+        }
+        .map { competitorsResults ->
+            CompetitorsResultsUIState(
+                competitorsResults = competitorsResults.sortedByDescending { it.second }
             )
+        }
+        .onEach { _isLoading.update { false } }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5_000),
+            initialValue = CompetitorsResultsUIState(listOf())
+        )
 
-    private val _isSearching = MutableStateFlow(false)
-    val isSearching = _isSearching.asStateFlow()
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
 
     fun onSearchQueryChange(query: String) {
         _searchQuery.value = query
